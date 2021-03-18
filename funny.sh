39cbfdd4a30938c321a6d2d5b4b120e42cc1d82b
saurabhsinghmqgit <80448137+saurabhsinghmqgit@users.noreply.github.com>
2021-03-19 00:14:22 +1100

    Update funny.sh

--- funny.sh
+++ funny.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [[ $# -eq 0 ]]; then
-        echo "This is not funny"
-        exit 1
+if [[ $# -eq 0 ]]; then #if conditions checks weather the arguments are equal to zero or not 
+        echo "This is not funny" # return the statement in double quotes if the IF statement is true
 else
-        echo "This is funny"
+        echo "This is funny" #returns the statement in double quotes if the IF statement is not true 
+        
 
 fi
