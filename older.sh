4701c447f2f02e1dec463123db3d7621e772df31
saurabhsinghmqgit <80448137+saurabhsinghmqgit@users.noreply.github.com>
2021-03-19 00:08:37 +1100

    Update older.sh

--- older.sh
+++ older.sh
@@ -1,1 +1,2 @@
-find -type f -printf '%T+ %p\n' | sort | head -n 1
+find -type f -printf '%T+ %p\n' | sort | head -n 1 
+# to find the oldest file, -type f is for searching file type, head -n 1 gives out the first file, printf will print the result  
